9f5cbb4ec3a0f4ee596117c3f2bffe03b1eb7b35
Marcello Airlangga Saputra <marcelloairlangga18@gmail.com>
2026-07-03 19:04:23 +0700

    controller, KnowledgeController Update

--- kms_java/src/controller/KnowledgeController.java
+++ kms_java/src/controller/KnowledgeController.java
@@ -18,7 +18,34 @@ public class KnowledgeController {
     }
 
     public boolean tambahPutusan(String[] data) {
-        return false;
+        try {
+            String nomorPerkara = data[0];
+            String pengadilan = data[1];
+            String tanggalPutusan = data[2];
+            String namaHakim = data[3];
+            String namaTerdakwa = data[4];
+            int umurTerdakwa = Integer.parseInt(data[5]);
+            String jenisNarkotika = data[6];
+            double beratBarangBukti = Double.parseDouble(data[7]);
+            String pasalDilanggar = data[8];
+            String peranTerdakwa = data[9];
+            int vonisHukuman = Integer.parseInt(data[10]);
+            double vonisDenda = Double.parseDouble(data[11]);
+
+            Putusan p = new Putusan(nomorPerkara, pengadilan, tanggalPutusan, namaHakim,
+                    namaTerdakwa, umurTerdakwa, jenisNarkotika, beratBarangBukti,
+                    pasalDilanggar, peranTerdakwa, vonisHukuman, vonisDenda);
+            repository.simpan(p);
+            view.tampilkanPesan("Data putusan berhasil ditambahkan. Total data: " + repository.getTotalData());
+            return true;
+
+        } catch (NumberFormatException e) {
+            view.tampilkanPesan("Gagal menambah data: format angka tidak valid.");
+            return false;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            view.tampilkanPesan("Gagal menambah data: data tidak lengkap.");
+            return false;
+        }
     }
 
     public ArrayList<Putusan> cariPutusan(String keyword, String mode) {
