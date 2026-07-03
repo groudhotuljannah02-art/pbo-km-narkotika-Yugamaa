67b9ec055fd4abdb5c15fcfa347690778599cc5c
Ghina Roudhotul Jannah <groudhotuljannah02@gmail.com>
2026-07-03 15:07:18 +0700

    Up model, KnowledgeRepository.java

---
+++ kms_java/src/model/KnowledgeRepository.java
@@ -0,0 +1,44 @@
+package model;
+
+import java.util.ArrayList;
+
+public class KnowledgeRepository {
+
+    private ArrayList<Putusan> daftarPutusan;
+
+    public KnowledgeRepository() {
+        this.daftarPutusan = new ArrayList<>();
+    }
+
+    public void simpan(Putusan p) {
+        daftarPutusan.add(p);
+    }
+
+    public Putusan cariByNomor(String nomor) {
+        for (Putusan p : daftarPutusan) {
+            if (p.getNomorPerkara().equalsIgnoreCase(nomor)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Putusan> cariByNama(String nama) {
+        ArrayList<Putusan> hasil = new ArrayList<>();
+        for (Putusan p : daftarPutusan) {
+            if (p.getNamaTerdakwa().toLowerCase().contains(nama.toLowerCase())) {
+                hasil.add(p);
+            }
+        }
+        return hasil;
+    }
+
+    public ArrayList<Putusan> getDaftarSemua() {
+        return daftarPutusan;
+    }
+
+    public int getTotalData() {
+        return daftarPutusan.size();
+    }
+}
+
